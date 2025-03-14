bb10829c818f04a598d777941e457b5719ae635a
Chris Ware <c.p.ware@hotmail.com>
2025-03-14 12:02:15 -0700

    ParallaxArea done

--- src/components/home-page/ParallaxArea/ParallaxArea.tsx
+++ src/components/home-page/ParallaxArea/ParallaxArea.tsx
@@ -5,6 +5,28 @@ import ParallaxImage from "@/components/shared/ParallaxImage/ParallaxImage";
 import styles from "./ParallaxArea.module.css";
 import LayoutWrapper from "@/components/shared/LayoutWrapper";
 import Img1 from "../../../../public/images/parallax.jpg";
+const data = [
+  {
+    id: 1,
+    title: "High Quality Products",
+  },
+  {
+    id: 2,
+    title: "Individual Advice",
+  },
+  {
+    id: 3,
+    title: "Professional Assembly",
+  },
+  {
+    id: 4,
+    title: "Uncomplicated Shipping",
+  },
+  {
+    id: 5,
+    title: "Persoanl Service",
+  },
+];
 
 export default function ParallaxArea() {
   return (
@@ -16,8 +38,26 @@ export default function ParallaxArea() {
         <div className={styles.content}>
           <div className={styles.contentChildren}>
             <h2 className={styles.heading}>
-              Parallax Area Parallax Area Parallax Area
+              Your <span className={styles.span}>thunder trail</span>
+              experience
             </h2>
+            <div className={styles.bottom}>
+              <div className={styles.left}>
+                <p className={styles.copy}>
+                  With our passion and enthusiasm for cycling, we offer high
+                  quality and exceptional bikes for riders of all levels.
+                </p>
+              </div>
+              <div className={styles.right}>
+                <ul className={styles.dataBox}>
+                  {data.map((x) => (
+                    <li key={x.id} className={styles.title}>
+                      <span className={styles.index}>{x.id}</span> {x.title}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </LayoutWrapper>
