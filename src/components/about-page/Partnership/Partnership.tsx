ddcbb20469f4994745775051ebafa530d1d638ec
Chris Ware <c.p.ware@hotmail.com>
2025-03-17 13:27:35 -0700

    Partnership done

---
+++ src/components/about-page/Partnership/Partnership.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import styles from "./Partnership.module.css";
+import LayoutWrapper from "@/components/shared/LayoutWrapper";
+import ParallaxImage from "@/components/shared/ParallaxImage/ParallaxImage";
+import Img1 from "../../../../public/images/partnership.jpg";
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+export default function Partnership() {
+  const [lettersRef, setLettersRef] = useArrayRef();
+  const triggerRef = useRef(null);
+
+  function useArrayRef(): [
+    React.MutableRefObject<HTMLSpanElement[]>,
+    (ref: HTMLSpanElement) => void
+  ] {
+    const lettersRef = useRef<HTMLSpanElement[]>([]);
+    lettersRef.current = [];
+    return [lettersRef, (ref) => ref && lettersRef.current.push(ref)];
+  }
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  const text =
+    "The partnership with our customers does not end with the purchase of the product, it really begins there.";
+
+  useEffect(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: triggerRef.current,
+        scrub: 0.5,
+        start: "top center",
+        end: "bottom center",
+        markers: false,
+      },
+    });
+
+    lettersRef.current.forEach((letter, index) => {
+      tl.to(
+        letter,
+        {
+          color: "#ffffff",
+          duration: 0.2,
+        },
+        index * 0.015
+      );
+    });
+
+    return () => {
+      tl.scrollTrigger?.kill();
+    };
+  }, [lettersRef]);
+
+  return (
+    <section className={styles.container} ref={triggerRef}>
+      <LayoutWrapper>
+        <div className={styles.content}>
+          <div className={styles.left}>
+            <div className={styles.imgContainer}>
+              <ParallaxImage src={Img1} alt='' />
+            </div>
+          </div>
+          <div className={styles.right}>
+            <h2 className={styles.heading}>
+              {text.split("").map((letter, index) => (
+                <span
+                  key={index}
+                  className={styles.revealText}
+                  ref={setLettersRef}
+                >
+                  {letter}
+                </span>
+              ))}
+            </h2>
+          </div>
+        </div>
+      </LayoutWrapper>
+    </section>
+  );
+}
