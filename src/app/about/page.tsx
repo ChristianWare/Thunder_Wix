bef4f5dcf48a45ede5e2b55dcf4e7f1bb94f2c80
Chris Ware <c.p.ware@hotmail.com>
2025-03-17 13:40:27 -0700

    About page almost done

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,11 @@
 import AboutHero from "@/components/about-page/AboutHero/AboutHero";
 import Explain from "@/components/about-page/Explain/Explain";
 import Partnership from "@/components/about-page/Partnership/Partnership";
+import CategorySection from "@/components/home-page/CategorySection/CategorySection";
+import ParallaxArea from "@/components/home-page/ParallaxArea/ParallaxArea";
+import FinalCTA from "@/components/shared/FinalCTA/FinalCTA";
+import Footer from "@/components/shared/Footer/Footer";
+import SignUp from "@/components/shared/SignUp/SignUp";
 
 export default function AboutPage() {
   return (
@@ -8,6 +13,11 @@ export default function AboutPage() {
       <AboutHero />
       <Explain />
       <Partnership />
+      <ParallaxArea />
+      <CategorySection />
+      <FinalCTA />
+      <SignUp />
+      <Footer />
     </main>
   );
 }
